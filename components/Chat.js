751a620b5bb8e0d84f398150c0824a3b52eea317
JokaMilen <77240807+JokaMilen@users.noreply.github.com>
2021-08-11 23:46:16 +0200

    installed Gifted Chat library ,created a chat interface for your chat app (text input for typing msg and speech bubbles for sent and received messages)

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
 
   componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+    this.setState({
+      messages: [
+        {
+          _id: 2,
+          text: name + ' has entered the chat',
+          createdAt: new Date(),
+          system: true,
+        },
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        }
+
+      ]
+    });
   }
 
   componentDidUpdate() {
@@ -14,22 +42,26 @@ export default class Chat extends React.Component {
     this.props.navigation.setOptions({ title: name });
   }
 
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
   render() {
     return (
-      <View style={[styles.container, { backgroundColor: this.props.route.params.backgroundColor }]}>
-        <Button
-          title="Go to start"
-          onPress={() => this.props.navigation.navigate("Start")}
+      <View style={{ flex: 1, backgroundColor: this.props.route.params.backgroundColor }}>
+        <GiftedChat
+          messages={this.state.messages}
+          renderUsernameOnMessage={true}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+            name: this.props.route.params.name
+          }}
         />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
     )
   };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
